265bf2f0e489954727ce9069d686ce6b95c71c81
Biniam Asnake <biniamasnake@gmail.com>
2023-03-20 19:41:34 +0100

    created springboot project

---
+++ src/main/java/com/act/moneytransferapi/MoneyTransferApiAct23Application.java
@@ -0,0 +1,13 @@
+package com.act.moneytransferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferApiAct23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTransferApiAct23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/act/moneytransferapi/MoneyTransferApiAct23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.act.moneytransferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferApiAct23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
