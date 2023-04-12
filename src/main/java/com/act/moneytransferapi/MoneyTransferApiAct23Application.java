0bc9d347ba584336da9782cf3a322b80e320eb42
Biniam Asnake <biniamasnake@gmail.com>
2023-04-12 15:59:36 +0200

    Enabled CORS for all headers and origins to allow the webapp in github pages to work

--- src/main/java/com/act/moneytransferapi/MoneyTransferApiAct23Application.java
+++ src/main/java/com/act/moneytransferapi/MoneyTransferApiAct23Application.java
@@ -2,6 +2,9 @@ package com.act.moneytransferapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoneyTransferApiAct23Application {
@@ -10,4 +13,18 @@ public class MoneyTransferApiAct23Application {
         SpringApplication.run(MoneyTransferApiAct23Application.class, args);
     }
 
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+//						.allowedOrigins(“http://localhost:8080“, “http://actmta.com");
+            }
+        };
+    }
 }
